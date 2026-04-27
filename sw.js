79b1aba1c41e32c30a0635e567f38fb5e600bd1b
Jes-win-hac-ker <jeswinjoy654@gmail.com>
2026-04-27 21:58:52 +0530

    Update service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v-scanner-v1';
+const CACHE_NAME = 'v-scanner-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -7,7 +7,18 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
